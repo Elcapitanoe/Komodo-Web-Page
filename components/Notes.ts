c7814b76db5faaaa86ba48a060e7655cfb2d28d9
Domi Adiwijaya <domy.adiwijaya@gmail.com>
2025-08-19 13:03:22 +0700

    docs: add warning about system corruption and data loss

--- components/Notes.ts
+++ components/Notes.ts
@@ -3,9 +3,9 @@ export function Notes(): HTMLElement {
   div.className = 'bg-[#FFF8E1] dark:bg-yellow-900/20 text-gray-700 dark:text-gray-300 rounded-xl shadow-sm border border-gray-200 dark:border-yellow-800/30 p-5 mb-8';
   
   div.innerHTML = `
-    <p class="font-semibold">Note:</p>
+    <p class="font-semibold">Warning:</p>
     <p>
-      This page refreshes every hour. However, due to GitHub limitations, the latest content may not appear immediately. You can check the timestamp of the latest deployment at the bottom of the page or visit the GitHub release page by clicking the “Previous Builds” button below.
+      Always perform a full backup before installing this module. We are not responsible for data loss, soft brick, or service disruption.
     </p>
   `;
 
